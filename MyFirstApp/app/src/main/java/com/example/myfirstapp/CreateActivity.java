d491c1f2644fe0d4a7d7ec5f5b47ba9f41404251
DCL280 <82668481+DCL280@users.noreply.github.com>
2021-04-19 14:16:54 -0700

    emoji keyboard cycles thru csv

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/CreateActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/CreateActivity.java
@@ -7,12 +7,73 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateActivity extends AppCompatActivity {
     Button home;
+    ArrayList<Button> displayedArray;
+    List<String[]> emojiList;
+    CSVReader reader;
+    ArrayList<String[]> emojiArray;
+    Button nextEmojiSet;
+    int emojiSetCount;
+    int lastSetCount;
+    int page;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        reader = new CSVReader(new InputStreamReader(getResources().openRawResource(R.raw.emoji_df)));
+        try {
+            emojiList = reader.readAll();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        emojiArray = new ArrayList<String[]>();
+        for(String[] i : emojiList) {
+            emojiArray.add(i);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        // setting emojis
+        displayedArray = new ArrayList<Button>();
+        displayedArray.add((Button)findViewById(R.id.button_emoji_1_create));
+        displayedArray.add((Button)findViewById(R.id.button_emoji_2_create));
+        displayedArray.add((Button)findViewById(R.id.button_emoji_3_create));
+        displayedArray.add((Button)findViewById(R.id.button_emoji_4_create));
+        displayedArray.add((Button)findViewById(R.id.button_emoji_5_create));
+        displayedArray.add((Button)findViewById(R.id.button_emoji_6_create));
+        displayedArray.add((Button)findViewById(R.id.button_emoji_7_create));
+        for(int i = 0; i < displayedArray.size(); i++){
+            displayedArray.get(i).setText(emojiArray.get(i+1)[0]);
+        }
+
+        // to next page of emojis
+        emojiSetCount = emojiArray.size() / displayedArray.size();
+        lastSetCount = emojiArray.size() - emojiSetCount*7;
+        nextEmojiSet = (Button)findViewById(R.id.button_next_emoji_set_create);
+        nextEmojiSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                page++;
+                if(page > emojiSetCount){
+                    page = 0;
+                }
+                if(page < emojiSetCount) {
+                    for (int i = 0; i < displayedArray.size(); i++) {
+                        displayedArray.get(i).setText(emojiArray.get(i + 1 + page * 7)[0]);
+                    }
+                } else {
+                    for (int i = 0; i < lastSetCount; i++) {
+                        displayedArray.get(i).setText(emojiArray.get(i + 1 + page * 7)[0]);
+                    }
+                }
+            }
+        });
 
         // to the home screen ie done creating
         home = (Button) findViewById(R.id.button_done_create);
