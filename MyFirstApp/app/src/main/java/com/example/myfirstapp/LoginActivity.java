2e2863e0653b7b6971534812882bc33e8824c9da
thatsmyonion <55523501+thatsmyonion@users.noreply.github.com>
2021-04-25 21:58:26 -0700

    login works now lol

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/LoginActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,13 +15,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class LoginActivity extends AppCompatActivity {
     Button register;
     Button login;
-    EditText email;
+    EditText email, password;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +44,9 @@ public class LoginActivity extends AppCompatActivity {
         if(get.getBooleanExtra("registered",false))
             DataHolder.setName(""); // TODO replace empty String with name from firebase
         // to the registration screen
+
         register = (Button)findViewById(R.id.button_no_account_login);
+
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,16 +56,50 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         // to the home screen ie logging in
-        email = (EditText)findViewById(R.id.editText_email_login);
-        login = (Button)findViewById(R.id.button_submit_login);
+        email = findViewById(R.id.editText_email_login);
+        password = findViewById(R.id.editText_pass_one_login);
+        fAuth = FirebaseAuth.getInstance();
+        login = findViewById(R.id.button_submit_login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // TODO conditionals for logging in
+                String Lemail = email.getText().toString().trim();
+                String Lpassword = password.getText().toString().trim();
                 Intent i = new Intent(getApplicationContext(), HomeActivity.class);
-                i.putExtra("from","login");
-                i.putExtra("username","");
-                startActivity(i);
+
+                if(TextUtils.isEmpty(Lemail)){
+                    email.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(Lpassword)){
+                    password.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    password.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(Lemail,Lpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(i);
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+
+//                i.putExtra("from","login");
+//                i.putExtra("username","");
+//                startActivity(i);
             }
         });
     }
